3a5f9ee975e77458567e3671b232c64c09f88930
JayRaj ❤️ <87997138+JayRaj8833@users.noreply.github.com>
2023-05-28 21:55:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ load_dotenv()
 
 
 # Mandatory variables for the bot to start
-API_ID = int(os.getenv("API_ID", "Your Api Id"))
-API_HASH = os.environ.get("API_HASH", "Your Api Hash")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
+API_ID = int(os.getenv("API_ID", "15415755"))
+API_HASH = os.environ.get("API_HASH", "dc4f9540457541bc4de6e4bfb630db71")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6240570615:AAG11PFpf7s7W72_aHKS3jCbBc-HWcmuus4")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("15415755")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Monfo url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Grolink")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://yash8833:OXZ1lnADF1s0qaO0@cluster0.6r2rrdp.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "15415755")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(Id Owned Id)
+ADMINS.append(1782059495)
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-938960731")) 
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Grolink_Official_Channel") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
-WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
+WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", 'https://i.ibb.co/XVydpmt/IMG-20230528-215336-407.jpg') # image when someone hit /start # image when someone hit /start
 LINK_BYPASS = "False"
