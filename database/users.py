c1bc3116ce74a400eea9696143b00576d0de1e7a
Ridzys96 <127649457+Ridzys96@users.noreply.github.com>
2023-06-08 20:55:59 +0530

    Update users.py

--- database/users.py
+++ database/users.py
@@ -20,7 +20,7 @@ async def get_user(user_id):
             "header_text": "",
             "footer_text": "",
             "username": None,
-            "base_site": "grolink.in",
+            "base_site": "ruban96.online",
             "banner_image": None,
             "is_banner_image": True,
             "is_username": True,
