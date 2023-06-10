9885fefd6996fe014124f5fed9d9c27c8d35196e
rubanserver <135997596+rubanserver@users.noreply.github.com>
2023-06-10 21:04:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("6094386527")] 
 ADMIN = ADMINS
 DATABASE_NAME = os.environ.get("DATABASE_NAME", "bynklink")
 DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://clientstamil4k:ruban9124@bynklink.mkhotjt.mongodb.net/?retryWrites=true&w=majority") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "6094386527")) 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "960432019")) 
 ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(960432019)
 #  Optionnal variables
